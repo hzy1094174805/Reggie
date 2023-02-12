e81ab85286d48f2bce479a5b8ad02079afa6fb13
HeZhongYu <ilovend@qq.com>
2023-02-12 09:24:22 +0800

    瑞吉项目环境搭建

--- reggie_take_out/src/main/java/com/itheima/reggie/entity/User.java
+++ reggie_take_out/src/main/java/com/itheima/reggie/entity/User.java
@@ -1,13 +1,13 @@
 package com.itheima.reggie.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 
@@ -21,7 +21,7 @@ import java.io.Serializable;
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
-@Accessors(chain = true)
+//@Accessors(chain = true)
 @TableName("user")
 @ApiModel(value="User对象", description="用户信息")
 public class User implements Serializable {
